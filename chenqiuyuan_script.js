2646acf57cf56de7631ffc6d3b92a33110226163
chenqiuyuan <380133194@qq.com>
2015-08-18 16:08:19 +0800

    蛤蛤

---
+++ chenqiuyuan_script.js
@@ -0,0 +1,11 @@
+//	var ex="yixinlin"；
+
+	function my(){
+		x=document.getElementById("demo");
+		x.innerHTML="Go and Learn with me!";
+	}/*how old are yo*/
+
+//	function var(){
+//		x=document.getElementById("here");
+//		x.innerHTML=ex；
+//	}
---
+++ hhh.php
@@ -0,0 +1,11 @@
+<<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		
+	</title>
+</head>
+<body>
+	<button type="button" onclick="alert('welcome')">here</button>
+</body> 
+</html>>
