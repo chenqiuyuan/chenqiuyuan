c73348f88e09c7cad1d4d8f5bd6e9ead13ea6f31
chenqiuyuan <380133194@qq.com>
2015-08-31 14:35:13 +0800

    just a try

--- chenqiuyuan.php
+++ chenqiuyuan.php
@@ -1,12 +1,12 @@
 <html>
 <head>
-<meta charset="UTF-8">
-<script type="text/javascript" src="chenqiuyuan_script.js"></script>
-</head>
+<meta charset="UTF-8">	
+<script type="text/jav	ascript" src="chenqiuyuan_script.js"></script>
+</head>	
 <body> 
 <p id="here">看我七十二变</p>
 <p id="demo">
-大家好,欢迎来到chenqiuyuan的地盘！
+	大家好,欢迎来到chenqiuyuan的地盘！
 </p>
 <button type="button" onclick="my()">这里</button>
 <button type="button" onclick="var()">点点我</button>
