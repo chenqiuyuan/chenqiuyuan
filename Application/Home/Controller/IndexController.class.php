a11cb5936a27c2b0c32680e85e69ae53b5bfcc97
chenqiuyuan <380133194@qq.com>
2015-09-20 17:36:58 +0800

    SAE config

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -3,7 +3,6 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
-    	$this->show("chenqiuyuan");
 		$this->display();        
     }
     public function hello($name='lalalaphp'){        
