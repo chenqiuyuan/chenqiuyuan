c5e6c6f88c880f3737e90b3fc8745c71b8d25829
chenqiuyuan <380133194@qq.com>
2015-09-21 12:23:59 +0800

    仔细想了想需求分析,现在还在业务需求分析的阶段

--- Application/Home/Conf/config_sae.php
+++ Application/Home/Conf/config_sae.php
@@ -1,6 +1,7 @@
-<?php 
-	return array( 
-    	'TMPL_PARSE_STRING'=>array( 
-    		'/Public/upload' => $st->getUrl('public','upload'), 
-		)   
-	)
+<?php
+return array(
+   'TMPL_PARSE_STRING'=>array(
+      // __PUBLIC__/upload  -->  /Public/upload -->http://appname-public.stor.sinaapp.com/upload
+   '/Public/upload'=>sae_storage_root('Public').'/upload'
+    )
+);
