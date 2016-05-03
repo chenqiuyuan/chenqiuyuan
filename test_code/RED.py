957144aacf30bf68f78b370abb7f20189fbf3d9b
陈秋远 <380133194@qq.com>
2016-05-03 10:06:57 +0800

    Merge remote-tracking branch 'origin/master'

--- test_code/RED.py
+++ test_code/RED.py
@@ -1,5 +1,4 @@
-import redis
-r = redis.Redis(host='localhost',port=6379,db=0)
-x = r.get('market')
-
-print 'chenqiuyuan'
+from WindPy import *
+w.start()
+data = w.wsd("510050.SH", "close", "2016-03-29", "2016-04-27")
+print 123
