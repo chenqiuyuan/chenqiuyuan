f5c3aa7dfafe9e2740a9edda31fd7b718f524852
陈秋远 <380133194@qq.com>
2016-05-09 10:07:44 +0800

    Merge remote-tracking branch 'origin/master'

--- test_code/RED.py
+++ test_code/RED.py
@@ -1,4 +1,10 @@
 from WindPy import *
 w.start()
-data = w.wsd("510050.SH", "close", "2016-03-29", "2016-04-27")
+data = w.wsd("510050.SH", "close", "2006-04-06", "2016-05-05", "Fill=Previous")
+num = len(data.Times)
+f = open("50ETF.csv","a+")
+for i in range(num):
+    line =  str(data.Times[i])[0:10] + "," +str(data.Data[0][i]) + "\n"
+    f.writelines(line)
+f.close()
 print 123
