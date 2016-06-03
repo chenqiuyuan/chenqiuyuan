32350285ccdf07b6d88fb5162d0afa44da2b7fd6
陈秋远 <380133194@qq.com>
2016-06-03 15:38:25 +0800

    Merge remote-tracking branch 'origin/master'

--- test_code/decorate.py
+++ test_code/decorate.py
@@ -1,38 +1,56 @@
-# -*- coding: utf-8 -*-
-import datetime
-import functools
-import os
-def log(text):
-    if callable(text):
-        @functools.wraps(text)
-        def wrapper(*args,**kw):
-            time1 = datetime.datetime.now()
-            print "begin call " + text.__name__ + "()"
-            text(*args,**kw)
-            time2 = datetime.datetime.now()
-            t = time2 - time1
-            print "end call " + text.__name__ + "()"
-            sleep(3)
-            print t.seconds
-        return wrapper
-    else:
-        def decorate(func):
-            @functools.wraps(func)
-            def wrapper(*args,**kw):
-                print "begin call " + func.__name__ + "() " + str(text)
-                func(*args,**kw)
-                print "end call " + func.__name__ + "() " + str(text)
-            return wrapper
-        return decorate
+import sys
+from PyQt4 import QtCore, QtGui
 
 
-@log
-def now1():
-    print 'hello'
+class Window(QtGui.QWidget):
 
-@log(233)
-def now2():
-    print "233"
+    def __init__(self):
+        QtGui.QWidget.__init__(self)
+        self.treeWidget = QtGui.QTreeWidget()
+        self.treeWidget.setHeaderHidden(True)
+        self.addItems(self.treeWidget.invisibleRootItem())
+        self.treeWidget.itemChanged.connect (self.handleChanged)
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.treeWidget)
+        self.setLayout(layout)
 
-now1()
-now2()
+    def addItems(self, parent):
+        column = 0
+        clients_item = self.addParent(parent, column, 'Clients', 'data Clients')
+        vendors_item = self.addParent(parent, column, 'Vendors', 'data Vendors')
+        time_period_item = self.addParent(parent, column, 'Time Period', 'data Time Period')
+
+        self.addChild(clients_item, column, 'Type A', 'data Type A')
+        self.addChild(clients_item, column, 'Type B', 'data Type B')
+
+        self.addChild(vendors_item, column, 'Mary', 'data Mary')
+        self.addChild(vendors_item, column, 'Arnold', 'data Arnold')
+
+        self.addChild(time_period_item, column, 'Init', 'data Init')
+        self.addChild(time_period_item, column, 'End', 'data End')
+
+    def addParent(self, parent, column, title, data):
+        item = QtGui.QTreeWidgetItem(parent, [title])
+        item.setData(column, QtCore.Qt.UserRole, data)
+        item.setChildIndicatorPolicy(QtGui.QTreeWidgetItem.ShowIndicator)
+        item.setExpanded (True)
+        return item
+
+    def addChild(self, parent, column, title, data):
+        item = QtGui.QTreeWidgetItem(parent, [title])
+        item.setData(column, QtCore.Qt.UserRole, data)
+        item.setCheckState (column, QtCore.Qt.Unchecked)
+        return item
+
+    def handleChanged(self, item, column):
+        if item.checkState(column) == QtCore.Qt.Checked:
+            print "checked", item, item.text(column)
+        if item.checkState(column) == QtCore.Qt.Unchecked:
+            print "unchecked", item, item.text(column)
+
+if __name__ == "__main__":
+
+    app = QtGui.QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(app.exec_())
